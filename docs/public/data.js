a72b825568f37adb6594390b5667d2aa3ed92941
Marvin Alegre <alegremrvn@protonmail.com>
2023-07-18 22:36:52 +0800

    content: update projects data

--- docs/public/data.js
+++ docs/public/data.js
@@ -1,25 +1,11 @@
 let projectsData = [
   {
-    projectName: "Philippine Earthquakes in 2022",
-    description: "A simple \"dashboard\" that summarizes the information on all earthquakes in the Philippines in the year 2022.",
-    tags: ["excel", "dashboard"],
-    location: "https://github.com/alegremrvn/philippine_earthquakes"
-  },
-  {
     projectName: 'alegremrvn.github.io',
     tags: [
       'html',
       'css',
-      'javascript'
+      'javascript',
+      'express'
     ]
   },
-  {
-    projectName: 'project3',
-    tags: [
-      'nodejs',
-      'express',
-      'react',
-      'mongodb'
-    ]
-  }
 ]
