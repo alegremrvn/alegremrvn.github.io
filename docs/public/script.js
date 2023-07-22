b73d0725cb262b179d5ec1adc6442e6371be73cb
Marvin Alegre <alegremrvn@protonmail.com>
2023-07-22 19:40:45 +0800

    feat: randomize projects for better sampling

--- docs/public/script.js
+++ docs/public/script.js
@@ -1,3 +1,5 @@
+shuffle(projectsData)
+
 let body = document.body
 let projectsDisplay = document.createElement('div')
 let navbar = document.createElement('nav')
@@ -199,4 +201,23 @@ function showPages() {
   } else {
     x.className = "";
   }
+}
+
+// copied this function from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  let currentIndex = array.length, randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
 }
